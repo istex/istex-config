b2c06efdcc407cba041620fd1c646dcf669ad23f
parentin <jean-joffrey.parentin@inist.fr>
2023-02-07 16:19:16 +0100

    feat(npm): add bin commands usable with npx

--- bin/es-mapping-output.js
+++ bin/es-mapping-output.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { buildMapping } = require('../src/buildMapping');
-const { outputJsonSync } = require('fs-extra');
+const { outputJsonSync, outputFileSync } = require('fs-extra');
 
 outputJsonSync('./output/mapping.json', buildMapping(), { spaces: '\x20\x20\x20\x20' });
+outputFileSync('./output/.gitignore', '*');
